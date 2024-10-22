001dffe71521e443a017a4e0d54599c14d850a7c
werrasonn <werrasonmulu7@gmail.com>
2024-10-22 21:38:35 +0300

    adding a dark button toggle

--- javascript.js
+++ javascript.js
@@ -5,6 +5,29 @@ function toggleMenu() {
     menu.classList.toggle("active"); // Toggle the "active" class
 }
 
+const toggleSwitchInput = document.getElementById('toggle-switch-input');
+const body = document.body;
+
+// Check for saved user preference
+if (localStorage.getItem('theme') === 'dark') {
+    body.classList.add('dark-mode');
+    toggleSwitchInput.checked = true;
+} else {
+    body.classList.add('light-mode');
+}
+
+// Toggle function
+toggleSwitchInput.addEventListener('change', () => {
+    body.classList.toggle('dark-mode');
+    body.classList.toggle('light-mode');
+    
+    // Save user preference
+    if (body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+});
 navLinks.forEach(link => {
   link.addEventListener('click', (event) => {
     event.preventDefault();
